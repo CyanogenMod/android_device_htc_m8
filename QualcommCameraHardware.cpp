8e703084d0238c58fd82435beaa54c6342f1764f
Vamshidhar Kondra <vkondra@codeaurora.org>
2010-11-02 15:46:54 +0530

    Camera: Fix the issue of mismatch in the  captured JPEG image and the postview.
    Padding and Crop values are incompatible with new jpeg downscaling method(change#34186).
    Adjusting them fixes the issue.
    
    Change-Id: Ic40e35b5ff19d5436b6cf40d693974a2161a3f87
    CRs-Fixed: 253598

--- QualcommCameraHardware.cpp
+++ QualcommCameraHardware.cpp
@@ -2050,6 +2050,8 @@ bool QualcommCameraHardware::native_jpeg_encode(void)
         int CbCrOffset = -1;
         if(mPreviewFormat == CAMERA_YUV_420_NV21_ADRENO)
             CbCrOffset = mCbCrOffsetRaw;
+        mCrop.in1_w = mDimension.orig_picture_dx - jpegPadding; // when cropping is enabled 
+        mCrop.in1_h = mDimension.orig_picture_dy - jpegPadding; // when cropping is enabled
 
         if (!LINK_jpeg_encoder_encode(&mDimension,
                                       thumbnailHeap,
@@ -4449,6 +4451,7 @@ void QualcommCameraHardware::notifyShutter(common_crop_t *crop, bool mPlayShutte
         if (crop->in1_w == 0 || crop->in1_h == 0) {
             // Full size
             if (mCurrentTarget == TARGET_MSM7627) {
+                jpegPadding = 0;
                 size.width = mDimension.ui_thumbnail_width;
                 size.height = mDimension.ui_thumbnail_height;
             } else {
@@ -4463,6 +4466,7 @@ void QualcommCameraHardware::notifyShutter(common_crop_t *crop, bool mPlayShutte
         } else {
             // Cropped
             if (mCurrentTarget == TARGET_MSM7627) {
+                jpegPadding = 8;
                 size.width = (crop->in1_w + jpegPadding) & ~1;
                 size.height = (crop->in1_h + jpegPadding) & ~1;
             } else {

a31d8323211e13581e4e45795a746ed0f1e6e1cc
Punit Soni <punits@codeaurora.org>
2012-02-27 15:24:04 -0800

    Camera: Change the scenemode names in HAL
    To comply with ICS API Specification,
    SCENE_MODE_AUTO changed to SCENE_MODE_ASD
    SCENE_MODE_OFF changed to SCENE_MODE_AUTO
    
    Change-Id: If145a76b4e79290160970a798157190a8138d043

--- QCameraHWI_Parm.cpp
+++ QCameraHWI_Parm.cpp
@@ -241,8 +241,8 @@ static const str_map iso[] = {
 };
 
 static const str_map scenemode[] = {
-    { CameraParameters::SCENE_MODE_OFF,            CAMERA_BESTSHOT_OFF },
-    { CameraParameters::SCENE_MODE_AUTO,           CAMERA_BESTSHOT_AUTO },
+    { CameraParameters::SCENE_MODE_AUTO,           CAMERA_BESTSHOT_OFF },
+    { CameraParameters::SCENE_MODE_ASD,            CAMERA_BESTSHOT_AUTO },
     { CameraParameters::SCENE_MODE_ACTION,         CAMERA_BESTSHOT_ACTION },
     { CameraParameters::SCENE_MODE_PORTRAIT,       CAMERA_BESTSHOT_PORTRAIT },
     { CameraParameters::SCENE_MODE_LANDSCAPE,      CAMERA_BESTSHOT_LANDSCAPE },
@@ -1103,7 +1103,7 @@ void QCameraHardwareInterface::initDefaultParameters()
 
     //Set Scene Mode
     mParameters.set(CameraParameters::KEY_SCENE_MODE,
-                    CameraParameters::SCENE_MODE_OFF);
+                    CameraParameters::SCENE_MODE_AUTO);
     mParameters.set(CameraParameters::KEY_SUPPORTED_SCENE_MODES,
                     mSceneModeValues);
 

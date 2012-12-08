adf29a0e532a5246fefb750957b556f7c61c979a
Ethan Chen <intervigil@gmail.com>
2012-12-08 14:46:17 -0800

    msm8960-common: enable kernel GTOD optimization
    Change-Id: Ibea67f48501b3fd18e5ef7ff2dd7bc8692759877

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -80,6 +80,9 @@ BOARD_EGL_CFG := device/htc/msm8960-common/configs/egl.cfg
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
 
+# Time
+KERNEL_HAS_GETTIMEOFDAY_HELPER := true
+
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
 BOARD_WLAN_DEVICE := qcwcn

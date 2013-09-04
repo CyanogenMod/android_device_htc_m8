9a31325989df7dafece53290a293d761a86ff591
Ethan Chen <intervigil@gmail.com>
2013-09-04 13:18:05 -0700

    msm8960-common: move media_profile to device repos
    * Different devices are capable of different front camera resolutions
    
    Change-Id: Ibab3cdc4e7802c94adf007f7f07b7b92b60d27e1

--- msm8960.mk
+++ msm8960.mk
@@ -93,7 +93,6 @@ PRODUCT_COPY_FILES += \
 
 # Media config
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml
 
 # Common build properties

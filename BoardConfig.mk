5a6dbae58430c8fd8759c60c77f985d2b7be4285
Jake Weinstein <xboxlover360@gmail.com>
2014-05-07 16:03:44 -0500

    m8: Switch to A15 flags, since Krait is closer to A15 than A9
    Seems to perform slightly better, could be placebo.
    
    Change-Id: If3aaf4552958db777bf1ce36669c850292f80c01

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -50,7 +50,7 @@ TARGET_HW_DISK_ENCRYPTION := true
 # Architecture
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_ARCH_VARIANT_CPU := cortex-a9
+TARGET_ARCH_VARIANT_CPU := cortex-a15
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true

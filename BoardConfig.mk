5320c0d3f1cadfdabf219e0dd887b197c9c2e86d
Jake Weinstein <xboxlover360@gmail.com>
2014-05-07 15:59:34 -0500

    m8: Update GPU configuration
    Add EGL Cache sizes and Adreno Renderscript override
    
    Change-Id: I1f5e3964aa88684a897549a6a41c5199736aaa77

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,6 +107,19 @@ TARGET_POWERHAL_VARIANT := qcom
 # RIL
 BOARD_PROVIDES_LIBRIL := true
 
+# Enables Adreno RS driver
+OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
+
+# Shader cache config options
+# Maximum size of the  GLES Shaders that can be cached for reuse.
+# Increase the size if shaders of size greater than 12KB are used.
+MAX_EGL_CACHE_KEY_SIZE := 12*1024
+
+# Maximum GLES shader cache size for each app to store the compiled shader
+# binaries. Decrease the size if RAM or Flash Storage size is a limitation
+# of the device.
+MAX_EGL_CACHE_SIZE := 2048*1024
+
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
 BOARD_WLAN_DEVICE := qcwcn

a39fd2fd638503fafa86aac99b86f19c7cce6c0c
invisiblek <dan.pasanen@gmail.com>
2014-05-06 23:17:34 -0500

    audio: remove unused audio_effects.conf
    Change-Id: I73d8b490559fe098ce09d5e323d92f94ca7b48c5

--- device.mk
+++ device.mk
@@ -30,7 +30,6 @@ $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_effects.conf:system/etc/audio_effects.conf \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
     $(LOCAL_PATH)/audio/soundimage/srsfx_trumedia_ext.cfg:system/etc/soundimage/srsfx_trumedia_ext.cfg \

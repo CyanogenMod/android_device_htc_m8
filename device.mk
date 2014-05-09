dcf55842d3aadecf994f2b163d211b7b843d5991
Nicholas Flintham <nick@flinny.org>
2014-05-09 16:05:42 +0100

    FM : Add FM service back and set permissions and props
    Change-Id: I6067de62a3f4aa294cdba8bea3c7470943cd6b49

--- device.mk
+++ device.mk
@@ -42,7 +42,8 @@ PRODUCT_PACKAGES += \
 # Qcom init scripts for /etc
 PRODUCT_PACKAGES += \
    init.qcom.bt.bluedroid.sh \
-   init.qcom.bt.sh
+   init.qcom.bt.sh \
+   init.qcom.fm.sh
 
 # Permissions
 PRODUCT_COPY_FILES += \

424fc739eb18d64c01e6598a5030927c90cadc46
Arun Manohar Manoharan <email2arunmanohar@gmail.com>
2014-05-14 15:32:53 +0530

    Update and rename carbon.mk to cm.mk

--- carbon.mk
+++
@@ -1,6 +0,0 @@
-$(call inherit-product, device/htc/m8/full_m8.mk)
-
-# Inherit some common CARBON stuff.
-$(call inherit-product, vendor/carbon/config/common_phone.mk)
-
-PRODUCT_NAME := carbon_m8
---
+++ cm.mk
@@ -0,0 +1,6 @@
+$(call inherit-product, device/htc/m8/full_m8.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+PRODUCT_NAME := cm_m8

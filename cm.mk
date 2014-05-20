78399c60aab5c7d85ba53c6100746a0bbbe2e4e3
task650 <task650@gmail.com>
2014-05-20 08:43:45 -0700

    Remove unused

--- cm.mk
+++
@@ -1,9 +0,0 @@
-$(call inherit-product, device/htc/m8/full_m8.mk)
-
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-PRODUCT_NAME := cm_m8

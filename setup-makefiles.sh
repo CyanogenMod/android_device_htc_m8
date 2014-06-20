34a463cd3c74c69c6d2783e13d2bc652495e2370
Phillip Killewald <phillip.killewald@gmail.com>
2014-06-19 21:05:51 -0400

    Updated location of libtime_genoff.so
    Change-Id: Id64e1d4ceba9f4d0b6f8fe105d19f3a7d4a4674d

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -102,7 +102,7 @@ ifneq (\$(filter m8,\$(TARGET_DEVICE)),)
 include \$(CLEAR_VARS)
 LOCAL_MODULE := libtime_genoff
 LOCAL_MODULE_OWNER := htc
-LOCAL_SRC_FILES := proprietary/vendor/lib/libtime_genoff.so
+LOCAL_SRC_FILES := proprietary/lib/libtime_genoff.so
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES

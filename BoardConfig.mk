56c0017ff98413d8f0c5ee351e35edba639400e6
Dan Pasanen <dan.pasanen@gmail.com>
2014-06-23 09:22:26 -0500

    BoardConfig: remove unused (and incorrect) flag
    Change-Id: I1958199076b4e7b5be37f77260c819d814de9c69

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,7 +43,6 @@ TARGET_BOARD_PLATFORM_GPU := qcom-adreno330
 # Architecture
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_ARCH_VARIANT_CPU := cortex-a9
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true

9c186c6d76dc24caddb7ca62b45c1831df0b12aa
Randall Rushing <randall.rushing@gmail.com>
2014-06-24 20:09:51 -0700

    GPE IR hal implementation. Ported from M7 work.
    Using SmartRemote app, the standard in user IR remote apps, it was
    confirmed IR code recored fails using AOSP IR hal implementation.
    Using the GPE HTC IR hal, with associated resources, blast as well
    as record works. There appears to be no conflicts having both
    implementations on the device at the same time.
    
    Change-Id: I2a97112a977d98ef601ce4e0f3922f3f7cd5e680

--- device.mk
+++ device.mk
@@ -31,6 +31,14 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_AAPT_CONFIG := hdpi xhdpi xxhdpi
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# HTC IR Blaster resources
+PRODUCT_PACKAGES += \
+    CIRModule \
+    htcirlibs \
+    cir \
+    cir_fw_update \
+    libhtcirinterface_jni
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \

350cc742786948d3b1d03a71a4cf23d202e931b7
William C. Garrison III <bill@cs.pitt.edu>
2014-07-31 19:20:04 -0400

    Dotcase: add scrolling caller name to ringing screen
    PS1: By xda's David 617
    PS2: Cleaned up spaces and removed debugging logging
    PS3: Renamed some vars for clarity, moved callerTicker into status
         class
    PS4: Added method to normalize characters
    PS5: Updated letter sprites to be more readable, only check name once
         per draw
    PS6: Minor adjustments
    PS7: Add extra space between loop, handle short names properly
    PS8: Fix weird ticker (add pauses on loop)
    PS9: Pause slightly longer when a call first arrives
    
    Change-Id: I2d038adeb904197217e565fdfc47608b3ecb1dc9

--- dotcase/src/org/cyanogenmod/dotcase/DotcaseStatus.java
+++ dotcase/src/org/cyanogenmod/dotcase/DotcaseStatus.java
@@ -42,7 +42,9 @@ public class DotcaseStatus {
 
     private boolean ringing = false;
     private int ringCounter = 0;
-    private String phoneNumber = "";
+    private String callerNumber = "";
+    private String callerName = "";
+    private int callerTicker = 0;
     private boolean torchStatus = false;
     private boolean alarmClock = false;
 
@@ -100,16 +102,38 @@ public class DotcaseStatus {
         ringCounter++;
     }
 
+    synchronized void startRinging(String number, String name) {
+        callerName = name;
+        startRinging(number);
+    }
+
     synchronized void startRinging(String number) {
         ringing = true;
         resetTimer = true;
         ringCounter = 0;
-        phoneNumber = number;
+        callerNumber = number;
+        callerTicker = -6;
     }
 
     synchronized void stopRinging() {
         ringing = false;
-        phoneNumber = "";
+        callerNumber = "";
+        callerName = "";
+    }
+
+    synchronized int callerTicker() {
+        if (callerTicker <= 0) {
+            return 0;
+        } else {
+            return callerTicker;
+        }
+    }
+
+    synchronized void incrementCallerTicker() {
+        callerTicker++;
+        if (callerTicker >= callerName.length()) {
+            callerTicker = -3;
+        }
     }
 
     synchronized void startAlarm() {
@@ -142,8 +166,12 @@ public class DotcaseStatus {
         return notifications.isEmpty();
     }
 
-    synchronized String getPhoneNumber() {
-        return phoneNumber;
+    synchronized String getCallerName() {
+        return callerName;
+    }
+
+    synchronized String getCallerNumber() {
+        return callerNumber;
     }
 
     synchronized List<Notification> getNotifications() {

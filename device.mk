ff356d9ae1c09594b642388c6012875d576f8552
Ethan Chen <intervigil@gmail.com>
2014-09-05 12:00:28 -0700

    m8: Use extended audio platform to provide speaker backend
    Change-Id: I8f1d04df822b3c3fc0884bb05344874411600370

--- device.mk
+++ device.mk
@@ -91,6 +91,7 @@ PRODUCT_PACKAGES += \
 # Audio configuration
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
     $(LOCAL_PATH)/audio/soundimage/srsfx_trumedia_ext.cfg:system/etc/soundimage/srsfx_trumedia_ext.cfg \

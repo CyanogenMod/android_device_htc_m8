1bcb8dfd7644684f431ecf9013c99cb95420a4a3
Dan Pasanen <dan.pasanen@gmail.com>
2014-11-13 20:43:10 -0600

    DO NOT MERGE: fixes for build
    * androidboot.selinux=permissive
    * Remove overlay items that are not yet implemented
    * Disable props that are too big
    * Don't ship HCE permission yet
    
    Change-Id: Ia46b46e9b0fb7968144b2a6fa852f256cfaea3a4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,7 +53,7 @@ TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
 COMMON_GLOBAL_CFLAGS += -DHTCLOG
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 ehci-hcd.park=3 zcache
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 ehci-hcd.park=3 zcache androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
@@ -146,12 +146,10 @@ BOARD_SEPOLICY_UNION += \
     drmserver.te \
     file_contexts \
     file.te \
-    hci_init.te \
     healthd.te \
     init_shell.te \
     init.te \
     keystore.te \
-    kickstart.te \
     mediaserver.te \
     rild.te \
     surfaceflinger.te \

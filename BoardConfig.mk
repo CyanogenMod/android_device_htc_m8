d445747823da84e31ad79d03c0c955fd906c1167
Dan Pasanen <dan.pasanen@gmail.com>
2014-11-19 20:27:02 -0600

    m8: use device-specific sepolicies
    Change-Id: Ia46b46e9b0fb7968144b2a6fa852f256cfaea3a4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,7 +53,7 @@ TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
 COMMON_GLOBAL_CFLAGS += -DHTCLOG
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 ehci-hcd.park=3 zcache
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 ehci-hcd.park=3 zcache androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
@@ -137,30 +137,31 @@ TARGET_USES_WCNSS_CTRL := true
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_AP := "ap"
 
+
 # SELinux
 BOARD_SEPOLICY_DIRS += \
     device/htc/m8/sepolicy
 
 BOARD_SEPOLICY_UNION += \
-    app.te \
-    bluetooth.te \
+    cir_fw_update.te \
     device.te \
-    domain.te \
-    drmserver.te \
     file_contexts \
-    file.te \
-    hci_init.te \
-    healthd.te \
+    hcheck.te \
     init_shell.te \
     init.te \
-    keystore.te \
-    kickstart.te \
     mediaserver.te \
+    mm-qcamerad.te \
+    mpdecision.te \
     rild.te \
-    surfaceflinger.te \
-    system.te \
+    rmt_storage.te \
+    su_daemon.te \
+    sysinit.te \
+    tee.te \
+    thermal-engine.te \
+    time_daemon.te \
     ueventd.te \
-    wpa.te
+    vold.te \
+    wcnss-service.te
 
 # Webkit
 ENABLE_WEBGL := true

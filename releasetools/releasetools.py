ba7ec1831c8385a0d857670911b6d03fc95c704b
Dan Pasanen <dan.pasanen@gmail.com>
2014-12-14 16:33:52 -0600

    m8: add assert on bootloader version
    * Per Steve - HTC turned on continuous splash and the new kernel expects it
    * Without an updated bootloader, recovery will fail
    
    Change-Id: I6ef41d706445e26385137deefb7898cc66e0bcbc

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -16,8 +16,27 @@
 
 """ Custom OTA commands for m8 devices """
 
+import common
+import re
+import os
+
 def FullOTA_InstallEnd(info):
   info.script.Mount("/system")
   info.script.AppendExtra('assert(run_program("/system/bin/makelinks.sh") == 0);')
   info.script.AppendExtra('ifelse(is_substring("0P6B20000", getprop("ro.boot.mid")), run_program("/sbin/sh", "-c", "busybox sed -i \'s/ro.com.google.clientidbase=android-google/ro.com.google.clientidbase=android-verizon/g\' /system/build.prop"));')
   info.script.Unmount("/system")
+
+def FullOTA_Assertions(info):
+  AddBootloaderAssertion(info, info.input_zip)
+
+def IncrementalOTA_Assertions(info):
+  AddBootloaderAssertion(info, info.target_zip)
+
+def AddBootloaderAssertion(info, input_zip):
+  android_info = input_zip.read("OTA/android-info.txt")
+  m = re.search(r"require\s+version-bootloader\s*=\s*(\S+)", android_info)
+  if m:
+    bootloaders = m.group(1).split("|")
+    if "*" not in bootloaders:
+      info.script.AssertSomeBootloader(*bootloaders)
+    info.metadata["pre-bootloader"] = m.group(1)

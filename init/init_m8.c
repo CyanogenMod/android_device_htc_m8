8a52f96705807ecdf9a7ad8a2869a647c90633ea
Simon Sickle <simon@simonsickle.com>
2014-12-14 19:05:50 -0600

    m8whl: Update fingerprint/description/unused from latest OTA
    Signed-off-by: Simon Sickle <simon@simonsickle.com>
    
    Change-Id: I98a27fd7e459ad0caa48f6c456f849bbb02e7824

--- init/init_m8.c
+++ init/init_m8.c
@@ -81,14 +81,13 @@ void vendor_load_properties()
     } else if (strstr(bootmid, "0P6B70000")) {
         /* m8spr (m8whl) */
         cdma_properties("1", "8", "310120", "Sprint");
-        property_set("ro.build.fingerprint", "htc/sprint_wwe/htc_m8whl:4.4.2/KOT49H/326598.8:user/release-keys");
-        property_set("ro.build.description", "1.54.651.8 CL326598 release-keys");
+        property_set("ro.build.fingerprint", "htc/sprint_wwe/htc_m8whl:4.4.4/KTU84P/393205.2:user/release-keys");
+        property_set("ro.build.description", "3.30.651.2 CL393205 release-keys");
         property_set("ro.product.device", "htc_m8whl");
         property_set("ro.build.product", "htc_m8whl");
         property_set("telephony.sms.pseudo_multipart", "1");
-        property_set("ro.ril.oem.ecclist", "911");
         property_set("ro.ril.enable.pre_r8fd=1", "1");
-        property_set("ro.ril.enable.sdr", "0");
+        property_set("ro.ril.oem.ecclist", "911");
         property_set("ro.ril.set.mtusize", "1422");
     } else {
         /* m8 */

327783863367ea65ecee2047072eb88676962cf5
Dan Pasanen <dan.pasanen@gmail.com>
2015-01-25 12:42:04 -0600

    releasetools: new baseband support
    * There's no good reason for them to have renamed mba.* other than
      to break our current symlink situation. The new firmware (a7b80e1.*)
      works perfectly fine symlinked as mba.* but we can't just assume
      everyone has it.
    
    * Add to our already hacky symlink script to conditionally create the
      mba.* symlinks.
    
    Change-Id: I79bcc9b9d827261b590c67c631f5b4cc5f7ca9bb

--- releasetools/makelinks.sh
+++ releasetools/makelinks.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+# Detect variant and create symlinks to its specific-blobs
 modelid=`getprop ro.boot.mid`
 
 case $modelid in
@@ -14,3 +15,23 @@ basedir="/system/blobs/$variant/"
 cd $basedir
 chmod 755 bin/*
 find . -type f | while read file; do ln -s $basedir$file /system/$file ; done
+
+
+# Create modem firmware links based on the currently installed modem
+mkdir -p /firmware/radio
+busybox mount -o shortname=lower -t vfat /dev/block/platform/msm_sdcc.1/by-name/radio /firmware/radio
+
+if [ -f "/firmware/radio/mba.mdt" ]; then
+  ln -s /firmware/radio/mba.mdt /system/vendor/firmware/mba.mdt
+  ln -s /firmware/radio/mba.b00 /system/vendor/firmware/mba.b00
+elif [ -f "/firmware/radio/a7b80e1.mdt" ]; then
+  ln -s /firmware/radio/a7b80e1.mdt /system/vendor/firmware/mba.mdt
+  ln -s /firmware/radio/a7b80e1.b00 /system/vendor/firmware/mba.b00
+fi
+
+if [ ! -f "/system/vendor/firmware/mba.mdt" ]; then
+  exit 1
+fi
+
+busybox umount /firmware/radio
+exit 0

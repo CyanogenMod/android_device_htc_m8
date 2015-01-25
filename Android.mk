2cd935bab292c5ed304e7d85d8ea22b5db87f6c2
Dan Pasanen <dan.pasanen@gmail.com>
2015-01-25 12:42:04 -0600

    releasetools: new baseband support
    * There's no good reason for them to have renamed mba.* other than
      to break our current symlink situation. The new firmware (a7b40e1.*)
      works perfectly fine symlinked as mba.* but we can't just assume
      everyone has it.
    
    * Add a hacky script to conditionally create the mba.* symlinks and
      also assert baseband versions since when we ship lollipop blobs for
      a variant, old radios are going to ruin lives.
    
    Change-Id: I79bcc9b9d827261b590c67c631f5b4cc5f7ca9bb

--- Android.mk
+++ Android.mk
@@ -37,18 +37,6 @@ $(MODEM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(MODEM_SYMLINKS)
 
-MBA_IMAGES := \
-    mba.b00 mba.mdt
-
-MBA_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(MBA_IMAGES)))
-$(MBA_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "MBA firmware link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /firmware/radio/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(MBA_SYMLINKS)
-
 ADSP_IMAGES := \
     adsp.b00 adsp.b01 adsp.b02 adsp.b03 adsp.b04 adsp.b05 adsp.b06 \
     adsp.b07 adsp.b08 adsp.b09 adsp.b10 adsp.b11 adsp.b12 adsp.mdt
---
+++ releasetools/modem.sh
@@ -0,0 +1,45 @@
+#!/sbin/sh
+
+set -e
+
+mkdir -p /firmware/radio
+busybox mount -o shortname=lower -t vfat /dev/block/platform/msm_sdcc.1/by-name/radio /firmware/radio
+
+radiover=`cat /firmware/radio/radiover.cfg | sed 's/SSD://g'`
+
+# Supported radios should break here
+case $radiover in
+    "1.05.20.0227_2")      break;;  # Sprint 4.4.2
+    "1.05.20.0321_2")      break;;  # Sprint 4.4.2
+    "1.08.20.0610")        break;;  # Sprint 4.4.2
+    "1.08.20.0612_4")      break;;  # Sprint 4.4.3
+    "1.08.20.0916")        break;;  # Sprint 4.4.4
+
+    "0.89.20.0222")        break;;  # Verizon 4.4.3
+    "0.89.20.0321")        break;;  # Verizon 4.4.3
+    "1.09.20.0702")        break;;  # Verizon 4.4.3
+    "1.09.20.0926")        break;;  # Verizon 4.4.4
+#    "1.12.20.0106")        break;;  # Verizon 5.0.1
+#    "1.12.20.1205")        break;;  # Verizon 5.0.1
+
+    "1.21.213311491.A04G") break;;  # GPE 5.0.1
+    *)
+        echo "ERROR: Baseband version $radiover is not valid! Please update your baseband."
+        exit 1;;
+esac
+
+if [ -f "/firmware/radio/mba.mdt" ]; then
+  ln -s /firmware/radio/mba.mdt /system/vendor/firmware/mba.mdt
+  ln -s /firmware/radio/mba.b00 /system/vendor/firmware/mba.b00
+elif [ -f "/firmware/radio/a7b40e1.mdt" ]; then
+  ln -s /firmware/radio/a7b40e1.mdt /system/vendor/firmware/mba.mdt
+  ln -s /firmware/radio/a7b40e1.b00 /system/vendor/firmware/mba.b00
+fi
+
+if [ ! -f "/system/vendor/firmware/mba.mdt" ]; then
+  echo "ERROR: Could not find a valid firmware image to symlink. Please ensure you have a supported baseband."
+  exit 1
+fi
+
+busybox umount /firmware/radio
+exit 0

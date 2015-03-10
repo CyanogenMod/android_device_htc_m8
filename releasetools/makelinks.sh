5bbe7e1631a3305ab14c9200444a7b5ab3bff4ed
Dan Pasanen <dan.pasanen@gmail.com>
2015-03-10 17:27:08 -0500

    makelinks: add links for other possible modem firmwares
    * If this becomes a recurring theme, I'm going to look at doing this
      a different way. For now, because I'm lazy, shipit.
    
    Change-Id: I45b57f4e1738e577a29934aa0beacd2ba704a5a7

--- releasetools/makelinks.sh
+++ releasetools/makelinks.sh
@@ -24,9 +24,15 @@ busybox mount -o shortname=lower -t vfat /dev/block/platform/msm_sdcc.1/by-name/
 if [ -f "/firmware/radio/mba.mdt" ]; then
   ln -s /firmware/radio/mba.mdt /system/vendor/firmware/mba.mdt
   ln -s /firmware/radio/mba.b00 /system/vendor/firmware/mba.b00
+elif [ -f "/firmware/radio/a7bc0e1.mdt" ]; then
+  ln -s /firmware/radio/a7bc0e1.mdt /system/vendor/firmware/mba.mdt
+  ln -s /firmware/radio/a7bc0e1.b00 /system/vendor/firmware/mba.b00
 elif [ -f "/firmware/radio/a7b80e1.mdt" ]; then
   ln -s /firmware/radio/a7b80e1.mdt /system/vendor/firmware/mba.mdt
   ln -s /firmware/radio/a7b80e1.b00 /system/vendor/firmware/mba.b00
+elif [ -f "/firmware/radio/a7b40e1.mdt" ]; then
+  ln -s /firmware/radio/a7b40e1.mdt /system/vendor/firmware/mba.mdt
+  ln -s /firmware/radio/a7b40e1.b00 /system/vendor/firmware/mba.b00
 fi
 
 if [ ! -f "/system/vendor/firmware/mba.mdt" ]; then

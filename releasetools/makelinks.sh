c6a564cce85370d1f14caddb9e2310afe0a0760a
Dan Pasanen <dan.pasanen@gmail.com>
2015-03-17 07:14:43 -0500

    makelinks: prefer mba before falling back to a7b*
    * Hopefully this is the last time we need to go through this
    
    * I had made the assumption that if a7b*.{b00,mdt} existed, there
      wouldn't be an mba.{b00,mdt} to symlink. That's not the case and
      apparently if there IS an mba.* pair, we should prefer to use that
    
    * Fall back to a7b* if no mba exists and take the one with the
      "highest" number (unless you're T-Mo) to (hopefully) future proof
      this clustermess
    
    Change-Id: I7f91df5a42a2988a99240f02384b9ff3f0b36e53

--- releasetools/makelinks.sh
+++ releasetools/makelinks.sh
@@ -8,6 +8,7 @@ modelid=`getprop ro.boot.mid`
 case $modelid in
     "0P6B20000") variant="vzw" ;;
     "0P6B70000") variant="spr" ;;
+    "0P6B13000") variant="gsm"; tmo="true" ;;
     *)           variant="gsm" ;;
 esac
 
@@ -21,12 +22,14 @@ find . -type f | while read file; do ln -s $basedir$file /system/$file ; done
 mkdir -p /firmware/radio
 busybox mount -o shortname=lower -t vfat /dev/block/platform/msm_sdcc.1/by-name/radio /firmware/radio
 
-if [ -f "/firmware/radio/a7bc0e1.mdt" ]; then
-  base="/firmware/radio/a7bc0e1"
-elif [ -f "/firmware/radio/a7b80e1.mdt" ]; then
+# Prefer an mba.* pair if one exists
+# If not, find the a7b* pair with the highest "number" (unless you're T-Mo)
+if [ "$tmo" == "true" ] && [ -f "/firmware/radio/a7b80e1.mdt" ]; then
   base="/firmware/radio/a7b80e1"
 elif [ -f "/firmware/radio/mba.mdt" ]; then
   base="/firmware/radio/mba"
+elif ls /firmware/radio/a7b*.mdt 1> /dev/null 2>&1; then
+  base=`ls /firmware/radio/a7b*.mdt | sort -r | head -1 | sed "s|.mdt||g"`
 fi
 
 ln -s $base.mdt /system/vendor/firmware/mba.mdt

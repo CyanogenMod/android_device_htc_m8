4283f1a610b020dc2c2233e5f6701efd3062c28c
Dan_htc_touch <damantea@gmail.com>
2015-03-20 00:26:08 -0600

    Should be futureproof makelinks.sh
    i noticed working radios had DEBUG1 string inside and ran with it
    
    Change-Id: I249e4e6a07f4f5aedcbea79be435916662ab6761

--- releasetools/makelinks.sh
+++ releasetools/makelinks.sh
@@ -8,7 +8,6 @@ modelid=`getprop ro.boot.mid`
 case $modelid in
     "0P6B20000") variant="vzw" ;;
     "0P6B70000") variant="spr" ;;
-    "0P6B13000") variant="gsm"; tmo="true" ;;
     *)           variant="gsm" ;;
 esac
 
@@ -22,15 +21,9 @@ find . -type f | while read file; do ln -s $basedir$file /system/$file ; done
 mkdir -p /firmware/radio
 busybox mount -o shortname=lower -t vfat /dev/block/platform/msm_sdcc.1/by-name/radio /firmware/radio
 
-# Prefer an mba.* pair if one exists
-# If not, find the a7b* pair with the highest "number" (unless you're T-Mo)
-if [ "$tmo" == "true" ] && [ -f "/firmware/radio/a7b80e1.mdt" ]; then
-  base="/firmware/radio/a7b80e1"
-elif [ -f "/firmware/radio/mba.mdt" ]; then
-  base="/firmware/radio/mba"
-elif ls /firmware/radio/a7b*.mdt 1> /dev/null 2>&1; then
-  base=`ls /firmware/radio/a7b*.mdt | sort -r | head -1 | sed "s|.mdt||g"`
-fi
+# choose highest numbered modem firmware with DEBUG1 string
+
+base='grep -l DEBUG1 /firmware/radio/*.b00 | sort -r | head -1 | sed "s|.b00||g"'
 
 ln -s $base.mdt /system/vendor/firmware/mba.mdt
 ln -s $base.b00 /system/vendor/firmware/mba.b00

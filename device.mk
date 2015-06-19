8a0e268d9528f075b237984d2f7b48ae24d51b4e
Steve Kondik <steve@cyngn.com>
2015-06-18 20:16:00 -0500

    m8: Don't need to copy FFMPEG config anymore
    Change-Id: Ibab51ac4742817a94abd43116266a5c323f1fbc0

--- device.mk
+++ device.mk
@@ -152,7 +152,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
 
 PRODUCT_COPY_FILES += \
-    frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml

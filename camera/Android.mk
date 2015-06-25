eb01a1854431775327e6acea87ee9e10de1bd441
Brinly Taylor <uberlaggydarwin@gmail.com>
2015-06-26 07:44:53 +0930

    m8: Reorganize camerawrapper makefile.
    Alphabetize
    
    Change-Id: Iefe0ff9b659ed73f4f1d0720623afe45ea855e74

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,14 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
+LOCAL_C_INCLUDES += \
+    system/media/camera/include
+
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
     libhardware liblog libcamera_client libutils
-
-LOCAL_C_INCLUDES += \
-    system/media/camera/include
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)

cc7d8223666365506ff92e29db7bb63b59d9ffc8
Ethan Chen <intervigil@gmail.com>
2015-07-03 19:47:03 -0700

    amplifier: Power off amps when idle
    Conflicts:
    	amplifier/audio_amplifier.c
    	amplifier/tfa9887.c
    
    Change-Id: I46a98e6cf57b7353d4417850bd262c5552766b6f

--- amplifier/tfa9887.h
+++ amplifier/tfa9887.h
@@ -111,6 +111,7 @@ enum {
 struct tfa9887_amp_t {
     int fd;
     bool is_right;
+    bool is_on;
     uint32_t mode;
     bool initializing;
     bool writing;
@@ -228,6 +229,7 @@ struct tfa9887_amp_t {
 #define I2S_MIXER_CTL "MI2S_RX Audio Mixer MultiMedia1"
 
 int tfa9887_open(void);
+int tfa9887_power(bool on);
 int tfa9887_set_mode(audio_mode_t mode);
 int tfa9887_close(void);
 

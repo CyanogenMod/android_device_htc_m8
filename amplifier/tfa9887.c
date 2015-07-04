2138bbf7c7a9b309583e98639cb3c15fd99f84fd
Ethan Chen <intervigil@gmail.com>
2015-07-04 00:55:10 -0700

    amplifier: Use VOIP configs for VOIP
    Conflicts:
    	amplifier/tfa9887.c
    
    Change-Id: I346a304029f9e7415231ef70550d3b297b5cf545

--- amplifier/tfa9887.c
+++ amplifier/tfa9887.c
@@ -54,6 +54,11 @@ const struct mode_config_t right_mode_configs[TFA9887_MODE_MAX] = {
         .preset = PRESET_VOICE_R,
         .eq = EQ_VOICE_R,
         .drc = DRC_VOICE_R
+    },
+    {   /* VOIP */
+        .config = CONFIG_VOIP_R,
+        .preset = PRESET_VOIP_R,
+        .eq = EQ_VOIP_R
     }
 };
 
@@ -75,6 +80,11 @@ const struct mode_config_t left_mode_configs[TFA9887_MODE_MAX] = {
         .preset = PRESET_VOICE_L,
         .eq = EQ_VOICE_L,
         .drc = DRC_VOICE_L
+    },
+    {   /* VOIP */
+        .config = CONFIG_VOIP_L,
+        .preset = PRESET_VOIP_L,
+        .eq = EQ_VOIP_L
     }
 };
 
@@ -211,8 +221,9 @@ static uint32_t get_mode(audio_mode_t mode)
         case AUDIO_MODE_RINGTONE:
             return TFA9887_MODE_RING;
         case AUDIO_MODE_IN_CALL:
-        case AUDIO_MODE_IN_COMMUNICATION:
             return TFA9887_MODE_VOICE;
+        case AUDIO_MODE_IN_COMMUNICATION:
+            return TFA9887_MODE_VOIP;
         case AUDIO_MODE_NORMAL:
         default:
             return TFA9887_MODE_PLAYBACK;
--- amplifier/tfa9887.h
+++ amplifier/tfa9887.h
@@ -45,6 +45,8 @@
 #define CONFIG_RING_L "/system/etc/tfa/ring_l.config"
 #define CONFIG_VOICE_R "/system/etc/tfa/voice.config"
 #define CONFIG_VOICE_L "/system/etc/tfa/voice_l.config"
+#define CONFIG_VOIP_R "/system/etc/tfa/voip.config"
+#define CONFIG_VOIP_L "/system/etc/tfa/voip_l.config"
 
 #define PRESET_PLAYBACK_R "/system/etc/tfa/playback.preset"
 #define PRESET_PLAYBACK_L "/system/etc/tfa/playback_l.preset"
@@ -52,6 +54,8 @@
 #define PRESET_RING_L "/system/etc/tfa/ring_l.preset"
 #define PRESET_VOICE_R "/system/etc/tfa/voice.preset"
 #define PRESET_VOICE_L "/system/etc/tfa/voice_l.preset"
+#define PRESET_VOIP_R "/system/etc/tfa/voip.preset"
+#define PRESET_VOIP_L "/system/etc/tfa/voip_l.preset"
 
 #define EQ_PLAYBACK_R "/system/etc/tfa/playbackwoofer.eq"
 #define EQ_PLAYBACK_L "/system/etc/tfa/playbackwoofer_l.eq"
@@ -59,6 +63,8 @@
 #define EQ_RING_L "/system/etc/tfa/ring_l.eq"
 #define EQ_VOICE_R "/system/etc/tfa/voice.eq"
 #define EQ_VOICE_L "/system/etc/tfa/voice_l.eq"
+#define EQ_VOIP_R "/system/etc/tfa/voip.eq"
+#define EQ_VOIP_L "/system/etc/tfa/voip_l.eq"
 
 #define DRC_PLAYBACK_R "/system/etc/tfa/playbackwoofer.drc"
 #define DRC_PLAYBACK_L "/system/etc/tfa/playbackwoofer_l.drc"
@@ -78,6 +84,7 @@ enum {
     TFA9887_MODE_PLAYBACK = 0,
     TFA9887_MODE_RING,
     TFA9887_MODE_VOICE,
+    TFA9887_MODE_VOIP,
     TFA9887_MODE_MAX,
 };
 

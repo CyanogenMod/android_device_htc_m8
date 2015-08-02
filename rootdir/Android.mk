5248a2cd546520a8a456f47f1de6834f68a0520d
Brinly Taylor <uberlaggydarwin@gmail.com>
2015-08-02 11:48:17 +0930

    eyeul: do not build f2fs on Mac
    This is a terrible hack but it will have to do as OSX will not compile without it.
    
    Silly Darwin doesn't have f2fs-tools
    
    Change-Id: I945688470bccfc739672b1bdbe60f2844c7a9292

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -21,7 +21,11 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.qcom
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
+ifeq ($(HOST_OS),linux)
 LOCAL_SRC_FILES    := etc/fstab.qcom
+else
+LOCAL_SRC_FILES    := etc/nof2fsfstab.qcom
+endif
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 

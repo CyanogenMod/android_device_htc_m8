8f224926136e8e73f9b7a914d252c5c1013a4c33
Michael Gernoth <michael@gernoth.net>
2015-12-14 17:50:00 -0600

    ril: Return static radio capability from getRadioCapability
    * Introduce common m8RIL
    
    Change-Id: I34374bbc27220b7eedbb60d3236750bb5ec400c4

---
+++ ril/telephony/java/com/android/internal/telephony/M8RIL.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2014, The CyanogenMod Project. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.telephony;
+
+import android.content.Context;
+import android.os.AsyncResult;
+import android.os.Message;
+
+/**
+ * RIL customization for One M8
+ *
+ * {@hide}
+ */
+public class M8RIL extends RIL {
+
+    public M8RIL(Context context, int networkMode, int cdmaSubscription) {
+        super(context, networkMode, cdmaSubscription, null);
+    }
+
+    public M8RIL(Context context, int preferredNetworkType,
+            int cdmaSubscription, Integer instanceId) {
+        super(context, preferredNetworkType, cdmaSubscription, instanceId);
+    }
+
+    @Override
+    public void getRadioCapability(Message response) {
+        riljLog("getRadioCapability: returning static radio capability");
+        if (response != null) {
+            Object ret = makeStaticRadioCapability();
+            AsyncResult.forMessage(response, ret, null);
+            response.sendToTarget();
+        }
+    }
+}

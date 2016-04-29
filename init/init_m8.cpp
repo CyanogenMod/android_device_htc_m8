107ed46397dbf6d3756a6a0d892d588dbf0bb909
Bruno Martins <bgcngm@gmail.com>
2016-04-29 10:28:29 -0700

    init: Import latest properties from M stock images
    * Based on build.prop from the following packages:
       * GSM variant - HTC package 5.07.1700.12
         (htc/m8_google/htc_m8:6.0/MRA58K.H12/666673:user/release-keys)
       * SPR variant - HTC package 6.20.651.3
         (htc/sprint_wwe/htc_m8whl:6.0/MRA58K/682910.3:user/release-keys)
       * VZW variant - HTC package 6.21.605.3
         (htc/HTCOneM8vzw/htc_m8wl:6.0/MRA58K/708002.3:user/release-keys)
    
    Change-Id: Id233ae9a225401ebaca5585106d75bec54e91aac

--- init/init_m8.cpp
+++ init/init_m8.cpp
@@ -46,14 +46,8 @@ void cdma_properties(char const default_cdma_sub[], char const default_network[]
 {
     property_set("ro.telephony.default_cdma_sub", default_cdma_sub);
     property_set("ro.telephony.default_network", default_network);
-
     property_set("telephony.lteOnCdmaDevice", "1");
-    property_set("ro.ril.svdo", "true");
-    property_set("ro.ril.disable.fd.plmn.prefix", "23402,23410,23411,23420");
     property_set("ro.ril.enable.sdr", "0");
-    property_set("ro.ril.enable.gea3", "1");
-    property_set("ro.ril.enable.a53", "1");
-    property_set("ro.ril.enable.r8fd=1", "1");
     property_set("persist.radio.snapshot_enabled", "1");
     property_set("persist.radio.snapshot_timer", "22");
 }
@@ -62,6 +56,7 @@ void gsm_properties(char const default_network[])
 {
     property_set("ro.telephony.default_network", default_network);
     property_set("telephony.lteOnGsmDevice", "1");
+    property_set("ro.ril.radio.svn", "1");
 }
 
 void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *board_type)
@@ -89,20 +84,25 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.ril.vzw.feature", "1");
         property_set("ro.ril.oem.ecclist", "911,*911,#911");
         property_set("ro.ril.enable.a52", "0");
+        property_set("ro.ril.enable.a53", "1");
         property_set("ro.ril.enable.dtm", "0");
+        property_set("ro.ril.enable.gea3", "1");
+        property_set("ro.ril.enable.r8fd", "1");
+        property_set("ro.ril.disable.fd.plmn.prefix", "23402,23410,23411,23420");
+        property_set("ro.ril.fast.dormancy.cdma.rule", "0");
         property_set("ro.ril.gprsclass", "12");
         property_set("ro.ril.att.feature", "0");
         property_set("ro.ril.enable.managed.roaming", "1");
         property_set("ro.ril.oem.show.act", "0");
+        property_set("ro.ril.pdpnumber.policy.roaming", "3");
         property_set("ro.ril.set.mtusize", "1428");
-        property_set("ro.ril.air.enabled", "1");
+        property_set("ro.ril.air.enabled", "0");
         property_set("ro.ril.wp.feature", "1");
         property_set("ro.cdma.data_retry_config", "max_retries=infinite,0,0,60000,120000,480000,900000");
         property_set("ro.gsm.data_retry_config", "max_retries=infinite,0,0,60000,120000,480000,900000");
         property_set("ro.gsm.2nd_data_retry_config", "max_retries=infinite,0,0,60000,120000,480000,900000");
-        property_set("ro.ril.gsm.to.lte.blind.redir", "1");
+        property_set("ro.ril.gsm.to.lte.blind.redir", "0");
         property_set("ro.config.svlte1x", "true");
-        property_set("ro.ril.def.agps.mode", "6");
         property_set("ro.telephony.get_imsi_from_sim", "true");
     } else if (strstr(bootmid, "0P6B70000")) {
         /* m8spr (m8whl) */
@@ -113,8 +113,7 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.build.description", "6.20.651.3 CL682910 release-keys");
         property_set("ro.product.device", "htc_m8whl");
         property_set("ro.build.product", "htc_m8whl");
-        property_set("telephony.sms.pseudo_multipart", "1");
-        property_set("ro.ril.enable.pre_r8fd=1", "1");
+        property_set("ro.ril.disable.fd.plmn.prefix", "23402,23410,23411,23420,23594,27202,27205");
         property_set("ro.ril.oem.ecclist", "911");
         property_set("ro.ril.set.mtusize", "1422");
         property_set("ro.cdma.home.operator.numeric", "310120");

87ee2f4d6df848e90139363da5cc14d0eee67eea
Bruno Martins <bgcngm@gmail.com>
2016-05-10 00:12:15 +0100

    m8: Convert to standalone init library
    Change-Id: I44689469b2ba84ff16a03a3ec52933765843fce3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,9 +54,9 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1073741824
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 13153337344
 
 # Vendor Init
+TARGET_INIT_VENDOR_LIB := libinit_m8
+TARGET_RECOVERY_DEVICE_MODULES := libinit_m8
 TARGET_UNIFIED_DEVICE := true
-TARGET_INIT_VENDOR_LIB := libinit_msm
-TARGET_LIBINIT_DEFINES_FILE := device/htc/m8/init/init_m8.cpp
 
 # Inherit from the proprietary version
 -include vendor/htc/m8/BoardConfigVendor.mk

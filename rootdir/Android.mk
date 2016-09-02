abeee8c4e03da63b918c89459f966c2c69d806eb
Bruno Martins <bgcngm@gmail.com>
2016-09-03 00:34:55 +0100

    m8: Guard variant rc script
    Change-Id: I7f443ba48b40cace98edc31fb6c657b12d8f96da

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,5 +1,6 @@
 LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
+
+ifeq ($(TARGET_DEVICE),m8)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.variant.rc
@@ -8,3 +9,5 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.variant.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
+endif

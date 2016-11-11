ae39452e31cf7dcd5d04cb790cefcd7467516d9c
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-11-10 20:03:15 -0600

    m8: remove variant rc script
    Change-Id: Iac2f78281b7710419080b1648dd2d9905e4d2c77

--- device.mk
+++ device.mk
@@ -19,7 +19,3 @@ DEVICE_PACKAGE_OVERLAYS += device/htc/m8/overlay
 
 # Inherit from m8-common
 $(call inherit-product, device/htc/m8-common/m8-common.mk)
-
-# Init
-PRODUCT_PACKAGES += \
-    init.variant.rc
